87f16d3ff2f57c363b8f5744075b8a030641dc91
Franco Brégoli <bregolif.fb@gmail.com>
2025-08-07 20:26:19 -0300

    incrementar latencia 30 mil veces
    acá si son 30 mil

--- src/core/reports_manager.py
+++ src/core/reports_manager.py
@@ -197,7 +197,7 @@ class ReportsManager:
                 f"Topología: {os.path.basename(config.topology_file)}\n"
                 f"Secuencia de funciones: {' -> '.join([f.value for f in self.config.functions_sequence])}\n"
                 f"Inicialización de Q-Values en un rango aleatorio entre 0 y 100\n"
-                # f"Latencia incrementada 10 veces\n"
+                f"Latencia incrementada 30.000 veces\n"
                 f"Sin exploración"
             )
 
