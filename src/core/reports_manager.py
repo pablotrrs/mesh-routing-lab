4f7cc42e8d24487263e05c1007f07d677fe42528
Franco Brégoli <bregolif.fb@gmail.com>
2025-08-15 20:18:35 -0300

    Revert "sin exploración"
    This reverts commit 1298f10b1663894c49cdd75cf9d84af0a78b7959.

--- src/core/reports_manager.py
+++ src/core/reports_manager.py
@@ -189,16 +189,15 @@ class ReportsManager:
                 # f"Int. reconexión fija: {config.reconnection_interval_ms} ms\n"
                 f"Int. desconexión media: {config.mean_disconnection_interval_ms} ms\n"
                 f"Int. reconexión media: {config.mean_reconnection_interval_ms} ms\n"
-                # f"Epsilon: 0.1\n"
-                # f"Epsilon decay: 0.999976975\n"
+                f"Epsilon: 0.1\n"
+                f"Epsilon decay: 0.999976975\n"
                 # f"Bonus for hop processing correct function: -0.1 \n"
                 # f"Bonus for hop finishing processing functions: -50 \n"
                 # f"Initial Q-Values: 100 \n"
                 f"Topología: {os.path.basename(config.topology_file)}\n"
                 f"Secuencia de funciones: {' -> '.join([f.value for f in self.config.functions_sequence])}\n"
                 f"Inicialización de Q-Values en un rango aleatorio entre 0 y 100\n"
-                f"Latencia incrementada 30.000 veces\n"
-                f"Sin exploración"
+                f"Latencia incrementada 30.000 veces"
             )
 
         os.makedirs(self.results_dir, exist_ok=True)
